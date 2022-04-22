6c7b4ba59f0a847ee46a6a233fe5059f3d214f9e
ceshmina <ceshmina@gmail.com>
2022-04-23 03:50:44 +0900

    #3: メールアドレスを追加し、SNSアイコンと合わせてMDIを使うように

--- src/components/SectionContact.vue
+++ src/components/SectionContact.vue
@@ -2,10 +2,13 @@
   <div class="section">
     <h1 class="section-title" id="contact-title">Contact</h1>
 
+    <div class="email">
+      <email size=25 fillColor="#2c3e50" /> shu&#64;ceshmina.jp
+    </div>
     <div class="sns-links">
       <template v-for="snsLink in snsLinks" :key="snsLink">
         <a :href="snsLink.link" target="_blank">
-          <img :src="getContactImagePath(snsLink.image)" />
+          <component :is="snsLink.tag" size=25 fillColor="#2c3e50" />
         </a>
       </template>
     </div>
@@ -13,19 +16,25 @@
 </template>
 
 <script>
+import Email from 'vue-material-design-icons/Email.vue'
+import Twitter from 'vue-material-design-icons/Twitter.vue'
+import Instagram from 'vue-material-design-icons/Instagram.vue'
+import Github from 'vue-material-design-icons/Github.vue'
+
 export default {
+  components: {
+    Email,
+    Twitter,
+    Instagram,
+    Github
+  },
   data() {
     return {
       snsLinks: [
-        {name: 'twitter', image: 'twitter-3.svg', link: 'https://twitter.com/ceshmina'},
-        {name: 'instagram', image: 'instagram-2-1.svg', link: 'https://instagram.com/ceshmina'},
-        {name: 'github', image: 'github-icon-1.svg', link: 'https://github.com/ceshmina'}
+        {name: 'twitter', tag: 'twitter', link: 'https://twitter.com/ceshmina'},
+        {name: 'instagram', tag: 'instagram', link: 'https://instagram.com/ceshmina'},
+        {name: 'github', tag: 'github', link: 'https://github.com/ceshmina'}
       ]
-    }
-  },
-  methods: {
-    getContactImagePath(image) {
-      return require('../assets/contact/' + image)
     }
   }
 }
