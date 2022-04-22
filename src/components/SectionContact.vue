a92f05538685f16e48e881695aa6dcc84a2c8655
ceshmina <ceshmina@gmail.com>
2022-04-23 04:06:36 +0900

    #3: メールとSNSアイコンのスタイル調整

--- src/components/SectionContact.vue
+++ src/components/SectionContact.vue
@@ -2,15 +2,18 @@
   <div class="section">
     <h1 class="section-title" id="contact-title">Contact</h1>
 
-    <div class="email">
-      <email size=25 fillColor="#2c3e50" /> shu&#64;ceshmina.jp
-    </div>
-    <div class="sns-links">
-      <template v-for="snsLink in snsLinks" :key="snsLink">
-        <a :href="snsLink.link" target="_blank">
-          <component :is="snsLink.tag" size=25 fillColor="#2c3e50" />
-        </a>
-      </template>
+    <div class="contacts">
+      <div class="email">
+        <email size=25 fillColor="#2c3e50" />
+        <p>shu&#64;ceshmina.jp</p>
+      </div>
+      <div class="sns-links">
+        <template v-for="snsLink in snsLinks" :key="snsLink">
+          <a :href="snsLink.link" target="_blank">
+            <component :is="snsLink.tag" size=25 fillColor="#2c3e50" />
+          </a>
+        </template>
+      </div>
     </div>
   </div>
 </template>
@@ -51,14 +54,31 @@ export default {
   font-size: 25px;
 }
 
+.contacts {
+  display: flex;
+  justify-content: center;
+}
+
+.email {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 150px;
+  margin: 30px 0;
+  padding-right: 30px;
+  font-size: 15px;
+  border-right: 1px solid #2c3e50;
+
+  p {
+    margin: 0 0 3px;
+  }
+}
+
 .sns-links {
   display: flex;
   justify-content: space-between;
-  width: 150px;
-  margin: 30px auto 0;
-
-  img {
-    width: 30px;
-  }
+  width: 120px;
+  margin: 30px 0;
+  padding-left: 30px;
 }
 </style>
